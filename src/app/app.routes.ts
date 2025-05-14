438b584932f93034456cb87c617848ee7192b41a
basmasalim <basmasalim030@gmail.com>
2025-05-14 18:18:43 +0300

    setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // ?====================> Auth Layout<=================
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/layouts/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
+      ),
+    canActivate: [() => {
+      return true
+    }],
+    children: [
+      { path: '', redirectTo: 'signin', pathMatch: 'full' },
+      {
+        path: 'signin',
+        loadComponent: () =>
+          import('./auth/components/sign-in/sign-in.component').then(
+            (m) => m.SignInComponent
+          ),
+      },
+      {
+        path: 'signup',
+        loadComponent: () =>
+          import('./auth/components/sign-up/sign-up.component').then(
+            (m) => m.SignUpComponent
+          ),
+      },
+    ],
+  },
+  // ?====================> Main Layout<=================
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/layouts/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
+      ), children: [
+        {
+          path: 'timeline',
+          loadComponent: () => import('./pages/main/timeline/timeline.component').then(
+            (m) => m.TimelineComponent)
+        },
+      ]
+  },
+  // ?====================> Wildcard Route<=================
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/main/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+];
---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const token = localStorage.getItem('token'); // null || token
+
+
+  if (token) {
+    router.navigate(['/timeline'])
+  }
+  return true;
+};
